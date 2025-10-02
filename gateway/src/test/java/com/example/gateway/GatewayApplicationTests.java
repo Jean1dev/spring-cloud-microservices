a7eff9c8768828f310b781e012638dc44d5c0cdc
Jeanluca <jeanlucafp@gmail.com>
2025-10-02 08:55:58 -0300

    feat: refactor test classes to extend ApplicationTestsNoAuth and add test security configuration

--- gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -1,12 +1,8 @@
 package com.example.gateway;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class GatewayApplicationTests {
+class GatewayApplicationTests extends ApplicationTestsNoAuth {
 
 	@Test
 	void contextLoads() {
--- gateway/src/test/java/com/example/gateway/apis/BaseApiConfigTest.java
+++ gateway/src/test/java/com/example/gateway/apis/BaseApiConfigTest.java
@@ -1,15 +1,12 @@
 package com.example.gateway.apis;
 
+import com.example.gateway.ApplicationTestsNoAuth;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class BaseApiConfigTest {
+class BaseApiConfigTest extends ApplicationTestsNoAuth {
 
     @Autowired
     private UserApiConfig userApiConfig;
